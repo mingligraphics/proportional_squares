2047205f78c32c207325ad5698a289ded6ffae01
MING LI <ming.li@wsj.com>
2025-05-04 15:42:40 -0400

    added animated squares on top of the original ones

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,17 @@
   import data from "./data/schools.json";
   import { scaleSqrt } from "d3-scale";
   import { extent, max, group } from 'd3-array';
+  import { tweened } from 'svelte/motion';
+  import { cubicOut } from "svelte/easing";
+  import { interpolate } from "d3-interpolate";
+  import { fly, fade, scale } from 'svelte/transition';
   console.log(data);
 
   let width = 700;
   let height = 500;
 
-  let background_Color = "#CED3DD";
+  let background_Color = "#C6E8EB";
+  let share_Color = "#683C8F";
   
   const margin = {
       top:0,
@@ -20,8 +25,6 @@
   let innerHeight = height - margin.top - margin.bottom;
 
   $: students_range = extent(data, d => d.intl_students);
-
-  $: console.log(students_range)
 
   $: rScale = scaleSqrt().domain(students_range).range(width > 590 ? [17, 206] : width > 470 ? [12, 160] : width > 370 ? [8, 120] : [6, 100]);
 
@@ -75,7 +78,27 @@ $: processedData = newData.map((d, i, arr) => {
   return result;
 });
 
-$: console.log(processedData)
+$: newData_before = processedData.map(item => ({
+  ...item,
+  case: "before",
+  share_china: 0  // add the new column
+}));
+
+$: newData_after = processedData.map(item => ({
+  ...item,
+  case: "after"   // add the new column
+}));
+
+$: console.log(tweenedPoints)
+
+$: tweenedPoints = tweened(newData_before, {
+      delay: 1000,
+      duration: 3000,
+      easing: cubicOut,
+      interpolate: (a, b) => interpolate(a, b)
+    });
+
+$:  tweenedPoints.set(newData_after);
 </script>
 
 <main>
@@ -86,6 +109,7 @@ $: console.log(processedData)
         <g class="inner-chart" transform="translate({margin.left}, {margin.top})">
           {#each processedData as d,i}
           <rect
+          in:scale={{ duration: 600, delay: i * 40, start: 0.1, easing: cubicOut }}
           x={
           d.column=="Column_1"?
           (max_Column1 - rScale(d.intl_students))/2 : 
@@ -105,7 +129,7 @@ $: console.log(processedData)
           stroke="none"
           stroke-width={1}
           fill={background_Color}
-          opacity={0.7}
+          opacity={0.5}
         />
         <text
         class="text-lables"
@@ -134,6 +158,32 @@ $: console.log(processedData)
         {d.code}
         </text>
         {/each}
+        {#each $tweenedPoints as d}
+        <rect
+        x={
+        d.column=="Column_1"?
+        (max_Column1 - rScale(d.intl_students))/2 : 
+        d.column=="Column_2"?
+        (max_Column1 + rScale(d.prev1Intl))/2 + gap:
+        d.column=="Column_3"?
+        (max_Column1 + rScale(d.prev2Intl))/2 + rScale(d.prev1Intl) + 2 * gap  
+        :(max_Column1 + rScale(d.prev3Intl))/2 + + rScale(d.prev2Intl)+ rScale(d.prev1Intl) + 3 * gap
+        }
+        y={
+        d.group=="A" ? 
+        (max_A - rScale(d.intl_students))/2 + (1 - d.share_china)*rScale(d.intl_students) : 
+        d.group=="B" ? 
+        max_A + row_width + (max_B - rScale(d.intl_students))/2 + (1 - d.share_china) * rScale(d.intl_students)  : 
+        max_A + max_B + 2 * row_width + (max_C - rScale(d.intl_students))/2 + (1 - d.share_china) * rScale(d.intl_students)
+        }
+        width={rScale(d.intl_students)}
+        height={d.share_china * rScale(d.intl_students)}
+        stroke="none"
+        stroke-width={0}
+        fill={share_Color}
+        opacity={0.4}
+      />
+      {/each}
         </g>
       </svg>
     </div>
